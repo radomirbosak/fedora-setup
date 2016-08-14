abfebe9374bfd33918664a3c678ddb14b74ad106
Radomír Bosák <radomir.bosak@gmail.com>
2016-08-14 22:01:02 +0200

    added fish completion for command g (previously go)

---
+++ fish/completions/g.fish
@@ -0,0 +1,19 @@
+set godir $HOME/.g
+
+function __fish_command_begins
+  set cmd (commandline -opc)
+  if [ (count $cmd) -eq 1 ]
+    return 0
+  else
+    return 1
+  end
+end
+
+complete -f -c g -n '__fish_command_begins' -a list    -d 'Lists links'
+complete -f -c g -n '__fish_command_begins' -a create  -d 'Create a new link'
+complete -f -c g -n '__fish_command_begins' -a back    -d 'Go back'
+complete -f -c g -n '__fish_command_begins' -a rm      -d 'Remove a link'
+complete -f -c g -n '__fish_command_begins' -a clip    -d 'Copy the link destination to clipboard'
+complete -f -c g -a '(ls $godir)'
+complete -f -c g -n '__fish_command_begins' -a nav     -d 'Navigate to (default)'
+complete -f -c g -n '__fish_use_subcommand nav' -a '(ls $godir)'
