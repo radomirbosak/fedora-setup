6227f8951a9272adc5562b697130cc609e88d37b
Radomír Bosák <radomir.bosak@gmail.com>
2016-12-01 18:08:54 +0100

    Change abbreviation for git remote -v

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -13,6 +13,6 @@ abbr -a gl 'git log'
 abbr -a glo 'git log --oneline'
 abbr -a gla 'git log --graph --oneline --decorate --all'
 abbr -a gp 'git push'
-abbr -a gr 'git remote -v'
+abbr -a grv 'git remote -v'
 
 abbr -a upx 'chmod u+x'
