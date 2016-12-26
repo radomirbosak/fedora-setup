e21bc4c2badc3304b729a7bca4de6d9d2a79f6d9
Radomír Bosák <radomir.bosak@gmail.com>
2016-12-26 18:49:00 +0100

    Bind ctrl+left/right to big-word-jumps

--- roles/fish/files/functions/fish_user_key_bindings.fish
+++ roles/fish/files/functions/fish_user_key_bindings.fish
@@ -1,3 +1,8 @@
 function fish_user_key_bindings
 	bind \cs 'commandline -i "$history[1]"'
+
+	# ctrl+left/right will do large jumps
+	# inspired by https://superuser.com/questions/409594/fish-control-left-control-right-keybindings
+	bind \e\[1\;5C forward-bigword
+	bind \e\[1\;5D backward-bigword
 end
